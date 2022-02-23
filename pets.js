a7e6902ff77856beb0516e482ec452e004a28d11
daniemoyo <daniemoyo@yahoo.com>
2022-02-23 16:58:59 -0600

    Added the Create Feature

--- pets.js
+++ pets.js
@@ -3,18 +3,45 @@ const fs = require("fs");
 const read = (index) => {
     fs.readFile('./pets.json', 'utf8' , (err, data) => {
         const parsedData = JSON.parse(data);
-        // console.log(Object.keys(parsedData).length);
-        if (err) {
-            console.error(err);
-            return
-        }
+        // console.log(parsedData);
+        if (err) throw err;
         index ? 
-            index > Object.keys(parsedData).length || index < 0 ? 
-                 console.error(`Usage: node pets.js read ${Object.keys(parsedData).length}`):
+            index > parsedData.length || index < 0 ? 
+                 console.error(`Usage: node pets.js read INDEX`):
                  console.log(parsedData[index]):
             console.log(parsedData);
     });
-}
+};
+
+const create = () =>{
+
+    if(process.argv.length <= 5){
+         console.error("Usage: node pets.js create AGE KIND NAME");
+         process.exit(1);
+    }else{
+        const [, , ,age, kind, name] = process.argv;
+        let pet = { 
+            age: Number(age),
+            kind: kind, 
+            name: name
+        };
+        fs.readFile('./pets.json', (err, data) => {
+            if (err) throw err;
+            let petObj = JSON.parse(data);
+            petObj.push(pet); 
+            
+            let data1 = JSON.stringify(petObj);
+        
+        fs.writeFile('./pets.json', data1, (err) => {
+            if (err) throw err;
+            console.log(pet);
+        });
+        
+    });
+        
+    }
+
+}; 
 const subCommand = process.argv[2];
 
 switch (subCommand) {
@@ -22,6 +49,7 @@ switch (subCommand) {
         read(process.argv[3]);
         break;
     case 'create':
+        create();
         break;
     case 'update':
         break;
@@ -29,6 +57,6 @@ switch (subCommand) {
         break;
     default:
         console.error("Usage: node pets.js [read | create | update | destroy]");
-        // process.exit(1);
+        process.exit(1);
 }
 
