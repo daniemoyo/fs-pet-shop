9c3b1fb270f46f83eec3a72c9ac05a8d0a2fcfa2
daniemoyo <daniemoyo@yahoo.com>
2022-03-10 09:00:34 -0600

    Added Basic Authentication

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -4,8 +4,14 @@ const app = express();
 const fs = require("fs");
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
-const { Pool } = require('pg');
 
+const basicAuth = require('express-basic-auth');
+app.use(basicAuth({
+    users: { 'admin':'meowmix'}
+}));
+
+
+const { Pool } = require('pg');
 const pool = new Pool({
     database: 'petshop'
 });
@@ -17,57 +23,33 @@ app.use(express.json());
 
 //Create a Pet
 app.post('/pets', (req, res) => {
-   if(Object.keys(req.body).length < 0 || typeof req.body.age !== 'number'){
-       res.status(400).send("Bad Request");
+    if(Object.keys(req.body).length < 0 || typeof req.body.age !== 'number'){
+        res.status(400).send("Bad Request");
     }else{
         const {age, name, kind} = req.body;
-
+        
         pool.query('INSERT INTO pets(age, name, kind) VALUES($1, $2, $3) RETURNING *',[age, name, kind], (err, result) => {
             console.log(result.rows);
             res.status(201).json(result.rows[0])
         });
     }
-  
+    
 });
 //Get all the Pets
 app.get('/pets', (req, res) => {
-   //Callback Function from readPets. Sends the response with the pets object in json
-   pool.query('SELECT * FROM pets', (err, result) => {
-    console.log(result.rows);
-    res.json(result.rows)
+    //Callback Function from readPets. Sends the response with the pets object in json
+    pool.query('SELECT * FROM pets', (err, result) => {
+        console.log(result.rows);
+        res.json(result.rows)
     });
 });
 
 //Look for a single pet
 app.get('/pets/:id', (req, res) => {
     //Callback Function from readPets. Sends the response with pets[index] in json
-     const id = Number(req.params.id);
-    if (id < 0 ||  typeof id === 'string'){
-        res.status(404).send('Not Found');
-    }else{
-        pool.query('SELECT * FROM pets WHERE id=$1',[id]
-        )
-        .then( (result)=> {
-            // console.log(result);
-            res.json(result.rows[0]);
-        })
-        .catch((err)=>{
-            res.sendStatus(500);
-        });
-    }
- });
- //Patch a pet
-app.patch('/pets/:index', (req, res) => {
-   const { age, name, kind } = req.body;
-   const id = Number(req.params.index);
-   const query = `
-        UPDATE pets SET
-            age  = COALESCE($1, age),
-            name = COALESCE($2, name),
-            kind = COALESCE($3, kind)
-        WHERE id= $4
-        RETURNING *`;
-    pool.query(query, [age,name,kind,id]
+    const id = Number(req.params.id);
+    
+    pool.query('SELECT * FROM pets WHERE id=$1',[id]
     )
     .then( (result)=> {
         // console.log(result);
@@ -76,31 +58,54 @@ app.patch('/pets/:index', (req, res) => {
     .catch((err)=>{
         res.sendStatus(500);
     });
+    
 });
-
- app.delete('/pets/:index', (req, res) => {
-    const id = Number(req.params.index);
+//Patch a pet
+app.patch('/pets/:id', (req, res) => {
+    const { age, name, kind } = req.body;
+    const id = Number(req.params.id);
     const query = `
+    UPDATE pets SET
+    age  = COALESCE($1, age),
+    name = COALESCE($2, name),
+    kind = COALESCE($3, kind)
+    WHERE id= $4
+    RETURNING *`;
+    pool.query(query, [age,name,kind,id]
+        )
+        .then( (result)=> {
+            // console.log(result);
+            res.json(result.rows[0]);
+        })
+        .catch((err)=>{
+            res.sendStatus(500);
+        });
+    });
+    
+    app.delete('/pets/:index', (req, res) => {
+        const id = Number(req.params.index);
+        const query = `
         DELETE FROM pets 
-         WHERE id= $1
-         RETURNING *`;
-     pool.query(query, [id]
-     )
-     .then( (result)=> {
-         // console.log(result);
-         res.json(result.rows[0]);
-     })
-     .catch((err)=>{
-         res.sendStatus(500);
-     });
- });
-
- app.use((req, res, next) => {
-    res.status(404).send("Not found");
-});
-
-app.listen(3000, ()=>{
-    console.log("Listening on Port: 3000");
-});
-
-module.exports = app;
+        WHERE id= $1
+        RETURNING *`;
+        pool.query(query, [id]
+            )
+            .then( (result)=> {
+                // console.log(result);
+                res.json(result.rows[0]);
+            })
+            .catch((err)=>{
+                res.sendStatus(500);
+            });
+        });
+        
+        
+        app.use((req, res, next) => {
+            res.status(404).send("Not found");
+        });
+        
+        app.listen(3000, ()=>{
+            console.log("Listening on Port: 3000");
+        });
+        
+        module.exports = app;
